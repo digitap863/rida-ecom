e3109a25775260bdff4942d11e11ce53e2644a80
Ajmal-Ali-k <ajmalali749@gmail.com>
2025-01-15 11:19:08 +0530

    url change

--- admin/src/api/url.js
+++ admin/src/api/url.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const url =import.meta.env.MODE === "development"?'http://localhost:8080/api/admin':'https://ridame.ae/api/admin'
+export const url =import.meta.env.MODE === "development"?'http://localhost:8080/api/admin':'https://rida-ecom-1.onrender.com'
 export const AxiosAdmin = axios.create({
     baseURL:url
 })
--- client/src/api/url.js
+++ client/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// export const url =import.meta.env.MODE === "development"?'http://localhost:8080/api':'https://ridame.ae/api'
-export const url ='http://localhost:8080/api'
+export const url =import.meta.env.MODE === "development"?'http://localhost:8080/api':'https://rida-ecom-1.onrender.com'
+// export const url ='http://localhost:8080/api'
 export const AxiosAdmin = axios.create({
     baseURL:url
 })
