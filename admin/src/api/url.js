02ec522bc6721b5dfa40f26814da24214545f7ea
Ajmal-Ali-k <ajmalali749@gmail.com>
2025-01-15 11:41:27 +0530

    fix

--- admin/src/api/url.js
+++ admin/src/api/url.js
@@ -1,6 +1,13 @@
 import axios from "axios"
 
-export const url =import.meta.env.MODE === "development"?'http://localhost:8080/api/admin':'https://rida-ecom-1.onrender.com'
+// export const url = process.env.NODE_ENV === "development" 
+//   ? 'http://localhost:8080/api/admin'
+//   : 'https://rida-ecom-1.onrender.com'
+// export const url = 'http://localhost:8080/api/admin'
+
+export const url = 'https://rida-ecom-1.onrender.com'
+
+
 export const AxiosAdmin = axios.create({
     baseURL:url
 })
