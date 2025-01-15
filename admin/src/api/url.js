0beabf5d5c0859f0f376d82783f4d0ad43b42658
Ajmal-Ali-k <ajmalali749@gmail.com>
2025-01-15 17:29:38 +0530

    admin url change

--- admin/src/api/url.js
+++ admin/src/api/url.js
@@ -3,9 +3,9 @@ import axios from "axios"
 // export const url = process.env.NODE_ENV === "development" 
 //   ? 'http://localhost:8080/api/admin'
 //   : 'https://rida-ecom-1.onrender.com'
-export const url = 'http://localhost:8080/api/admin'
+// export const url = 'http://localhost:8080/api/admin'
 
-// export const url = 'https://rida-ecom-1.onrender.com/api/admin'
+export const url = 'https://rida-ecom-1.onrender.com/api/admin'
 
 
 export const AxiosAdmin = axios.create({
