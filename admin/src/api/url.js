9bae73e63a4917e0372f4cb5454876da84c7f486
Ajmal-Ali-k <ajmalali749@gmail.com>
2025-02-22 10:55:34 +0530

    fix

--- admin/src/api/url.js
+++ admin/src/api/url.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const url = import.meta.env.MODE === "development" 
   ? 'http://localhost:8080/api/admin'
-  : '/api/admin'
+  : 'https://thermalparts.ae/api/admin'
 
 
 // export const url = 'https://rida-ecom-1.onrender.com/api/admin'
